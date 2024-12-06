6270472fc25efcd5b1b4763a5945e9517d596c61
Alexis MP <alexis.mp@gmail.com>
2024-12-06 10:01:58 +0000

    using "New App" to fix scaffolding

--- my-tennis-coach/app.py
+++ my-tennis-coach/app.py
@@ -6,12 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# Placeholder for personal analysis page.  This would require more details about the intended functionality
 @app.route("/personal_analysis")
 def personal_analysis():
    return render_template("personal_analysis.html")
 
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
-
